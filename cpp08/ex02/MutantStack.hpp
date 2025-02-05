6783ba9c19018f84adeb5ab458c5208c28c55be7
HalisBay <eminbay_58@hotmail.com>
2025-02-05 16:38:40 +0300

    🛌🏻㊗️

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -1,10 +1,7 @@
 #ifndef MUTANTSTACK_HPP
 #define MUTANTSTACK_HPP
 
-#include <algorithm>
-#include <iostream>
 #include <stack>
-#include <vector>
 
 template <typename T>
 class MutantStack : public std::stack<T>
@@ -13,32 +10,8 @@ public:
     typedef typename std::stack<T>::container_type::iterator iterator;
     typedef typename std::stack<T>::container_type::const_iterator const_iterator;
 
-    iterator begin()
-    {
-        std::vector<T> vector;
-        T top;
-        iterator ret;
-        int n = 0;
-
-        while (!this->empty())
-        {
-            top = this->top();
-            vector.push_back(top);
-            this->pop();
-            n++;
-        }
-        this->push(vector[n - 1]);
-        n--;
-        ret = this->c.begin();
-        while (n--)
-            this->push(vector[n]);
-        return ret;
-    }
-
-    iterator end()
-    {
-        return this->c.end();
-    }
+    iterator begin() { return this->c.begin(); }
+    iterator end() { return this->c.end(); }
 };
 
 #endif // MUTANTSTACK_HPP
